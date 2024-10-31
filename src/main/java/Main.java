5c4707e65f996aa7ad499c8d71662556e992c183
EoSeunghyo <djtmdgy123@naver.com>
2024-10-31 17:14:28 +0900

    전체 머지에서 승효파트 테스트 후 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,12 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        String id ="kang123";
+        //아래 함수 3개 테스트해보고 메인에서 지우면 됩니다. 흐진씨
+        String id ="kimsohee";
         String time = "202411111111";
-        //OrderManeger.getOrderFromUser(time, id);        //test용임. 머지해서 getOrderTime(), getOrderId()사용
+        //OrderManeger.getOrderFromUser(time, id);        //매개변수 test용임. 머지해서 getOrderTime(), getOrderId()로 받아오면됨.
         //OrderManeger.check_order_history_from_Admin();
-        OrderManeger.check_order_history_from_User(id);
+        //OrderManeger.check_order_history_from_User(id);   //매개변수 TEST용임. getOrderId()로 받아오면됨.
 
         //MenuManager.showMenu();
         System.out.println();
