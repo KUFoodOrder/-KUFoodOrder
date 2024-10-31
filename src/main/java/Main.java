58ac9ecbfcc514e07279e1f0800b847f7360f9b4
EoSeunghyo <djtmdgy123@naver.com>
2024-10-31 18:27:22 +0900

    고객 메인 메뉴 구현 후 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,14 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        //아래 함수 3개 테스트해보고 메인에서 지우면 됩니다. 흐진씨
-        String id ="kimsohee";
-        String time = "202411111111";
+        //String id ="kimsohee";
+        //String time = "202411111111";
+        //OrderManeger.Print_User_Main_Menu(time, id);
         //OrderManeger.getOrderFromUser(time, id);        //매개변수 test용임. 머지해서 getOrderTime(), getOrderId()로 받아오면됨.
         //OrderManeger.check_order_history_from_Admin();
         //OrderManeger.check_order_history_from_User(id);   //매개변수 TEST용임. getOrderId()로 받아오면됨.
 
-        //MenuManager.showMenu();
+        MenuManager.showMenu();
         System.out.println();
     }
 }
